9553caf76be6f6e1622fe79f4ff72bf5a418b77a
leekli <li.kirkham@live.com>
2022-02-23 18:09:05 +0000

    tidies up listen.js, removes the unused env-cmd package

--- listen.js
+++ listen.js
@@ -3,9 +3,6 @@
 const app = require("./app.js");
 require("./db/connection.js");
 
-// process.env.NODE_ENV = "development";
-console.log(process.env.NODE_ENV);
-
 const port = process.env.PORT || 3000;
 
 app.listen(port, (err) => {
