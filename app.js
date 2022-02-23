62c6b925c9b2500cd01e6687b5550dda4cd8dbe4
leekli <li.kirkham@live.com>
2022-02-23 13:42:38 +0000

    adds error handling into app.js

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const express = require("express");
 const cors = require("cors");
 const apiRouter = require("./routes/api.router.js");
 
+const {
+  handleCustomErrors,
+  handleMdbErrors,
+  handleServerErrors,
+  handle404s,
+} = require("./errors/errors.js");
+
 // Initalise server
 const app = express();
 
@@ -14,5 +21,11 @@ app.use(express.json());
 
 // Router Endpoints
 app.use("/api", apiRouter);
+
+// Error handling
+app.all("*", handle404s);
+app.use(handleCustomErrors);
+app.use(handleMdbErrors);
+app.use(handleServerErrors);
 
 module.exports = app;
